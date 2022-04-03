cdf15bc6435068b7d0b376acbc707166c3e60d25
moonbelle <71431806+moonbell-e@users.noreply.github.com>
2022-04-03 19:00:36 +0300

    added auto's upgrade fully and money system

--- LudumeDare50/Assets/Scripts/Aigerims/Economics/MoneySystem.cs
+++ LudumeDare50/Assets/Scripts/Aigerims/Economics/MoneySystem.cs
@@ -1,14 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class MoneySystem : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _moneyText;
-    [SerializeField] private int _moneyAmount;
     [SerializeField] private float _timer;
     [SerializeField] private float _currentTime;
+
+    public int MoneyAmount;
+
+    #region Singleton Init
+    private static MoneySystem _instance;
+    private void Awake() // Init in order
+    {
+        if (_instance == null)
+            Init();
+        else if (_instance != this)
+        {
+            Debug.Log($"Destroying {gameObject.name}, caused by one singleton instance");
+            Destroy(gameObject);
+        }
+    }
+    public static MoneySystem Instance // Init not in order
+    {
+        get
+        {
+            if (_instance == null)
+                Init();
+            return _instance;
+        }
+        private set { _instance = value; }
+    }
+    static void Init() // Init script
+    {
+        _instance = FindObjectOfType<MoneySystem>();
+        if (_instance != null)
+            _instance.Initialize();
+    }
+    #endregion
+
     private void Update()
     {
         if (_currentTime > 0)
@@ -16,14 +46,20 @@ public class MoneySystem : MonoBehaviour
         else
         {
             InscreaseMoney();
-            _moneyText.text = _moneyAmount.ToString();
             _currentTime = _timer;
         }
+
+        _moneyText.text = MoneyAmount.ToString();
     }
 
     private int InscreaseMoney()
     {
-        return _moneyAmount += 100;
+        return MoneyAmount += 100;
+    }
+
+    private void Initialize()
+    {
+        enabled = true;
     }
 
 }
